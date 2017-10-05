4ac98c1f5664221aa05e7997d35e3e18f7652828
Roger Ramirez <roger8849@gmail.com>
2017-10-05 17:04:32 -0500

    Partially using Serializaiton utils but anyway must be changed to strings as an Alternative.

--- vlc-multicast-client/src/main/java/com/javeriana/vlcmulticast/client/util/ObjectConverter.java
+++ vlc-multicast-client/src/main/java/com/javeriana/vlcmulticast/client/util/ObjectConverter.java
@@ -1,11 +1,8 @@
 package com.javeriana.vlcmulticast.client.util;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 
+import org.apache.commons.lang3.SerializationUtils;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -19,23 +16,32 @@ public class ObjectConverter {
   public static final Logger LOG = LoggerFactory.getLogger(ObjectConverter.class);
 
   public static byte[] fromMessageToByteData(MulticastMessage message) throws IOException {
-    LOG.debug("Converting message to byte data: {}", message);
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    ObjectOutputStream os = new ObjectOutputStream(outputStream);
-    os.writeObject(message);
-    byte[] byteArray = outputStream.toByteArray();
-    LOG.debug("Message successfuly converted. {}", byteArray);
-    return byteArray;
+    // LOG.debug("Converting message to byte data: {}", message);
+    // ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    // ObjectOutputStream os = new ObjectOutputStream(outputStream);
+    // os.writeObject(message);
+    // byte[] byteArray = outputStream.toByteArray();
+    // LOG.debug("Message successfuly converted. {}", byteArray);
+    // return byteArray;
+    return SerializationUtils.serialize(message);
   }
 
-  public static MulticastMessage fromByteDataToMessage(byte[] data)
-      throws IOException, ClassNotFoundException {
-    LOG.debug("Converting byte to multicast Message: {}", data);
-    ByteArrayInputStream in = new ByteArrayInputStream(data);
-    ObjectInputStream is = new ObjectInputStream(in);
-    MulticastMessage multicastObject = (MulticastMessage) is.readObject();
-    LOG.debug("Multicast object transformed: {}", multicastObject);
-    return multicastObject;
+  public static MulticastMessage fromByteDataToMessage(byte[] data) {
+    // LOG.debug("Converting byte to multicast Message: {}", data);
+    // ByteArrayInputStream in = new ByteArrayInputStream(data);
+    // ObjectInputStream is = null;
+    // try {
+    // is = new ObjectInputStream(in);
+    // } catch (IOException e) {
+    // }
+    // MulticastMessage multicastObject = null;
+    // try {
+    // multicastObject = (MulticastMessage) is.readObject();
+    // } catch (ClassNotFoundException | IOException e) {
+    // }
+    // LOG.debug("Multicast object transformed: {}", multicastObject);
+    // return multicastObject;
+    return SerializationUtils.deserialize(data);
   }
 
   public static String fromObjectToJsonString(Object object)
---
+++ vlc-multicast-client/src/main/java/com/javeriana/vlcmulticast/server/enumeration/MessageType.java
@@ -0,0 +1,5 @@
+package com.javeriana.vlcmulticast.server.enumeration;
+
+public enum MessageType {
+  ASKING, PARAMETERS_OF_START
+}
