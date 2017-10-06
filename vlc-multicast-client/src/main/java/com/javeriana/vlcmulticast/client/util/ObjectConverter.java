4138371a5701c5b0abd0438747a7eb4045bcb6d8
Roger Ramirez <roger8849@gmail.com>
2017-10-06 10:59:25 -0500

    Changing object Converter in client and server

--- vlc-multicast-client/src/main/java/com/javeriana/vlcmulticast/client/util/ObjectConverter.java
+++ vlc-multicast-client/src/main/java/com/javeriana/vlcmulticast/client/util/ObjectConverter.java
@@ -2,7 +2,6 @@ package com.javeriana.vlcmulticast.client.util;
 
 import java.io.IOException;
 
-import org.apache.commons.lang3.SerializationUtils;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -15,39 +14,49 @@ public class ObjectConverter {
 
   public static final Logger LOG = LoggerFactory.getLogger(ObjectConverter.class);
 
+  // public static byte[] fromMessageToByteData(MulticastMessage message) throws IOException {
+  // // LOG.debug("Converting message to byte data: {}", message);
+  // // ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+  // // ObjectOutputStream os = new ObjectOutputStream(outputStream);
+  // // os.writeObject(message);
+  // // byte[] byteArray = outputStream.toByteArray();
+  // // LOG.debug("Message successfuly converted. {}", byteArray);
+  // // return byteArray;
+  // return SerializationUtils.serialize(message);
+  // }
+
   public static byte[] fromMessageToByteData(MulticastMessage message) throws IOException {
-    // LOG.debug("Converting message to byte data: {}", message);
-    // ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    // ObjectOutputStream os = new ObjectOutputStream(outputStream);
-    // os.writeObject(message);
-    // byte[] byteArray = outputStream.toByteArray();
-    // LOG.debug("Message successfuly converted. {}", byteArray);
-    // return byteArray;
-    return SerializationUtils.serialize(message);
+    String jsonObject = fromObjectToJsonString(message);
+    byte[] data = jsonObject.getBytes();
+    return data;
   }
 
-  public static MulticastMessage fromByteDataToMessage(byte[] data) {
-    // LOG.debug("Converting byte to multicast Message: {}", data);
-    // ByteArrayInputStream in = new ByteArrayInputStream(data);
-    // ObjectInputStream is = null;
-    // try {
-    // is = new ObjectInputStream(in);
-    // } catch (IOException e) {
-    // }
-    // MulticastMessage multicastObject = null;
-    // try {
-    // multicastObject = (MulticastMessage) is.readObject();
-    // } catch (ClassNotFoundException | IOException e) {
-    // }
-    // LOG.debug("Multicast object transformed: {}", multicastObject);
-    // return multicastObject;
-    return SerializationUtils.deserialize(data);
+  // public static MulticastMessage fromByteDataToMessage(byte[] data)
+  // throws IOException, ClassNotFoundException {
+  // // LOG.debug("Converting byte to multicast Message: {}", data);
+  // // ByteArrayInputStream in = new ByteArrayInputStream(data);
+  // // ObjectInputStream is = new ObjectInputStream(in);
+  // // MulticastMessage multicastObject = (MulticastMessage) is.readObject();
+  // // LOG.debug("Multicast object transformed: {}", multicastObject);
+  // return SerializationUtils.deserialize(data);
+  // }
+  public static MulticastMessage fromByteDataToMessage(byte[] data)
+      throws IOException, ClassNotFoundException {
+    String jsonString = new String(data);
+    jsonString = jsonString.trim();
+    return fromJsonStringToMulticastMessage(jsonString);
   }
 
   public static String fromObjectToJsonString(Object object)
       throws JsonGenerationException, JsonMappingException, IOException {
     ObjectMapper mapper = new ObjectMapper();
     return mapper.writeValueAsString(object);
+  }
+
+  public static MulticastMessage fromJsonStringToMulticastMessage(String jsonObject)
+      throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    return mapper.readValue(jsonObject, MulticastMessage.class);
   }
 
   private ObjectConverter() {}
